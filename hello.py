ab9195268ae8ba8b3bc671266ea43ed265d76802
Jacek Czerwonka <jacekczerwonka@gmail.com>
2019-02-19 15:54:22 +0100

    second commit my scripts

---
+++ hello.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from math import *
+
+
+
+print('hello')
+
+print('----------')
+print('podaj liczbe')
+liczba = int(input())
+
+
+#licze
+print(liczba*liczba)
+print(2*liczba)
+
+#urojone
+print("\n---------\n")
+complex = 1+1j
+liczba+=1
+
+print(floor(abs(complex)))
+
+
